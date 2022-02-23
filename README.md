becf1014b3412ebceaa6cead8feb44459b03c0d6
Gibdos <simon.yousefi@protonmail.com>
2022-02-23 06:43:27 +0100

    Changed layout, +function createList, +styling

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # js_fizzbuzz
+
+## What is this?
+A simple FizzBuzz game with a number input and a <p> tag showing all numbers from 1 to the inputed number.
+
+Instead of a number it will print Fizz if the number is dividable by 3, Buzz if it is dividable by 5 and FizzBuzz if it is dividable by 3 and 5.
+
+## Example
+User inputs 15
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const numberInput = document.querySelector('#number');
-const para1 = document.querySelector('p');
+const numberInput = document.querySelector('#number'); // get the number input field
+const ulList = document.querySelector('ul');
+numberInput.value = ''
 
-numberInput.addEventListener('change', () => {
-  const numberValue = numberInput.value;
-for (let i = 1; i <= numberValue; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    para1.innerHTML += "FizzBuzz" + "<br />";
+numberInput.addEventListener('change', () => { // Check for a change in the number and do...
+  ulList.innerHTML = ''; // empty the list
+  const numberValue = numberInput.value; // Get the number value that was inputted
+for (let i = 1; i <= numberValue; i++) { // run a loop as long as the Int is lower than the inputted number
+  if (i % 3 === 0 && i % 5 === 0) { // if the value of i is divisible by 3 && 5
+    createList('FizzBuzz') // create a new li-tag with the content FizzBuzz
   } 
-  else if (i % 5 === 0) {
-    para1.innerHTML += "Buzz" + "<br />";
+  else if (i % 5 === 0) { // if the value of i is divisible by 5
+    createList('Buzz') // create a new li-tag with the content Buzz
   }
-  else if (i % 3 === 0) {
-    para1.innerHTML += "Fizz" + "<br />";
+  else if (i % 3 === 0) { // if the value of i is divisible by 3
+    createList('Fizz') // create a new li-tag with the content Fizz
   }
-  else {
-    para1.innerHTML += i + "<br />";
+  else { // if the value of i is neither divisible by 3 and / or (XOR) 5
+    createList(i) // create a new li-tag with the content i (number)
   }
 }
 });
+
+function createList(input) {
+      const createList = document.createElement('li'); // create a new li element
+      createList.textContent = input; // set the content of the li-tag
+      if (createList.textContent === 'Fizz') {
+        createList.style.backgroundColor = '#845ec2'
+      } else if (createList.textContent === 'Buzz') {
+        createList.style.backgroundColor = '#0081cf'
+      } else if (createList.textContent === 'FizzBuzz') {
+        createList.style.backgroundColor = '#008e9b'
+      }
+      ulList.appendChild(createList); // append the li-tag with content to the ul-tag
+}
